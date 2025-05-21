b5449a29920b4799d8b4ec47d6f6b135887ce361
justZa7 <mantha2116@gmail.com>
2025-05-21 19:04:48 +0700

    add new contract address

--- src/lib/contract.js
+++ src/lib/contract.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import NFTMINTER from "@/contractAbi/MyNFT.json"
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 
 export function getContact(signerOrPrivider) {
     return new ethers.Contract(CONTRACT_ADDRESS, NFTMINTER.abi, signerOrPrivider);
