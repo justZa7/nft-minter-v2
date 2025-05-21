99aa6d34aaf3741255902eacdc49bd3d019e152e
justZa7 <mantha2116@gmail.com>
2025-05-21 19:12:14 +0700

    try to use string straight

--- src/lib/contract.js
+++ src/lib/contract.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import NFTMINTER from "@/contractAbi/MyNFT.json"
 
-const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS.toString();
+const CONTRACT_ADDRESS = "0x449CA877B30E582FDCe8893CA8e3526619b154DE";
 
 export function getContact(signerOrPrivider) {
     return new ethers.Contract(CONTRACT_ADDRESS, NFTMINTER.abi, signerOrPrivider);
