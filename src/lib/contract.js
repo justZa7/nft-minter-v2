a1dbaed4e9f40f4a19875f1a085ac62cd5c137f3
justZa7 <mantha2116@gmail.com>
2025-05-21 19:09:44 +0700

    try fetch the address from env

--- src/lib/contract.js
+++ src/lib/contract.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import NFTMINTER from "@/contractAbi/MyNFT.json"
 
-const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS.toString();
 
 export function getContact(signerOrPrivider) {
     return new ethers.Contract(CONTRACT_ADDRESS, NFTMINTER.abi, signerOrPrivider);
