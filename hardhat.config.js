766b59f1685f9b6044e995218e90e4e6e94198c7
justZa7 <mantha2116@gmail.com>
2025-05-22 19:59:14 +0700

    deleting gasPrice on hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,6 @@ module.exports = {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
       chainId: 43113,
       accounts: [process.env.PRIVATE_KEY],
-      gasPrice: 225000000000, 
     }
   }
 };
